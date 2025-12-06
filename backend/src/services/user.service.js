f2ae79977f012ae9bb5fb5133cdea38a06fdf6bd
qctrung <lngquoctrung@gmail.com>
2025-12-06 07:05:23 +0700

    Add function to check if data has changed when updating profile

--- backend/src/services/user.service.js
+++ backend/src/services/user.service.js
@@ -1,4 +1,4 @@
-const { NotFoundError } = require("../core/error.response");
+const { NotFoundError, BadRequestError } = require("../core/error.response");
 const userModel = require("../models/user.model");
 
 class UserService {
@@ -9,9 +9,22 @@ class UserService {
     }
 
     updateProfile = async (userId, data) => {
-        const user = await userModel.findByIdAndUpdate(userId, data, { new: true }).select("-password -role -createdAt -updatedAt").lean();
-        if (!user) throw new NotFoundError({ message: "User not found" });
-        return user;
+        // Check if the old data and new data have changed?
+        const oldUserData = await userModel.findById(userId).select("-password -role -createdAt -updatedAt").lean();
+        if (!oldUserData) throw new NotFoundError({ message: "User not found" });
+
+        const isChange = Object.keys(data).some(key => data[key] !== oldUserData[key]);
+        if (!isChange) throw new BadRequestError({ message: "No data to update" });
+
+        // Update user
+        const newUser = await userModel.findByIdAndUpdate(userId, {
+            ...data,
+            updatedAt: Date.now(),
+        }, {
+            new: true
+        }).select("-password -role -createdAt -updatedAt").lean();
+        if (!newUser) throw new NotFoundError({ message: "User not found" });
+        return newUser;
     }
 }
 
